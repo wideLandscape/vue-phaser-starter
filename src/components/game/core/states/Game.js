d807ec5f6fd71a39e4e48945c2d2351b9c0e380f
Adriano Costa <iano76@gmail.com>
2018-03-10 16:28:43 +0100

    rename Game folder in game/core folder

--- src/components/game/core/states/Game.js
+++ src/components/game/core/states/Game.js
@@ -1,7 +1,7 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
-import { fetchWeather } from '../../api'
+import { fetchWeather } from '../../../../api'
 
 export default class extends Phaser.State {
   init () {
